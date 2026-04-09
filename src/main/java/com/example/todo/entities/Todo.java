150c100b5cc89e818a69cb869e4b7873f0829412
CavidHuseinov <Cavid.huseynovv28@gmail.com>
2026-04-09 21:51:40 +0400

    add empty conastructor automation

--- src/main/java/com/example/todo/entities/Todo.java
+++ src/main/java/com/example/todo/entities/Todo.java
@@ -1,13 +1,17 @@
 package com.example.todo.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
 @Table(name = "todos")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
